037f4b0fbb2971b4b70edc57654900e2176f366c
N-meapp <developersnme@gmail.com>
2025-10-14 06:22:23 +0530

    remove jade

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const connectDb = require('./config/db')
-var indexRouter = require('./routes/index');
+// var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const adminRouter = require('./routes/admin');
 const authRouter = require('./routes/auth')
@@ -14,6 +14,7 @@ const mongoose = require("mongoose");
 
 var app = express();
 connectDb()
+const PORT = process.env.PORT || 3000;
 
 app.use(cors({
   origin: "http://localhost:5173",
@@ -25,23 +26,22 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/admin', adminRouter);
 app.use('/auth', authRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler - UPDATED FOR REACT FRONTEND
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // Set locals, only providing error in development
   const isDevelopment = req.app.get('env') === 'development';
-  
+
   // Return JSON error response instead of rendering
   res.status(err.status || 500);
   res.json({
@@ -53,4 +53,8 @@ app.use(function(err, req, res, next) {
   });
 });
 
-module.exports = app;
+app.listen(PORT, () => {
+
+  console.log(`Server started on port ${PORT}`);
+
+});
--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
- res.json({ 
-    message: 'API is working!',
-    status: 'OK'
-  });
+  res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
