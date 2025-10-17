08b9f9a1a1a9cbea067f745bcaf6e1edde912341
N-meapp <developersnme@gmail.com>
2025-10-17 11:32:16 +0530

    firoz update

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ connectDb()
 const PORT = process.env.PORT || 3000;
 
 app.use(cors({
-  origin: ["https://dan-ecommerce.vercel.app", "http://localhost:5173"],
+  origin: "http://localhost:5173",
   credentials: true
 }));
 
